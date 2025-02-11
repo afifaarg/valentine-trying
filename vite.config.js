a335bd08dbe14041b2e7bb3d19539d2503b3ec5d
afifaarg <argoubafifa1@gmail.com>
2025-02-11 23:17:39 +0100

    deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  server: {
+    port: process.env.PORT || 5173, // Use PORT from environment or default to 5173
+    host: true, // Expose the app to external requests (0.0.0.0)
+  },
 })
